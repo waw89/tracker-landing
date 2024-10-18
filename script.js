8d02e2bee45da3522517db6b08a5058848c9523f
waw89 <lcontrerasperaza99@gmail.com>
2024-10-18 08:04:01 -0700

    added animations

--- script.js
+++ script.js
@@ -20,3 +20,51 @@ trackerImage.onclick = function() {
         trackerImage.classList.remove('moving-image'); 
     }, 10);
 };
+
+
+
+const headers = document.querySelectorAll('.buy-header'); 
+
+window.addEventListener('scroll', checkBoxes);
+
+
+checkBoxes(); 
+
+function checkBoxes (){
+    const triggerBottom = window.innerHeight/5 * 4;
+
+    headers.forEach(header => {
+
+        const headerTop = header.getBoundingClientRect().top; 
+        
+        
+        if(headerTop < triggerBottom){
+            header.classList.add('show')
+        } else{
+            header.classList.remove('show')
+        }
+    })
+}
+
+
+const steps = document.querySelectorAll('.step');
+
+window.addEventListener('scroll', checkStepBoxes);
+
+checkStepBoxes();
+
+function checkStepBoxes(){
+    const stepTriggerBottom = window.innerHeight / 5 * 4;
+
+    steps.forEach(step => {
+
+        const stepTop = step.getBoundingClientRect().top;
+
+        if(stepTop < stepTriggerBottom){
+            step.classList.add('show')
+        } else{
+            step.classList.remove('show')
+        }
+    })
+
+}
